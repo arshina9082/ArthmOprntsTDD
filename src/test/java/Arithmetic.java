079768fd4520a68993a7d69f42e1801752cf77df
ARSHINA HANIF DANGE <arshina775@gmail.com>
2021-08-17 02:07:51 +0530

    Commiting

---
+++ src/test/java/Arithmetic.java
@@ -0,0 +1,28 @@
+public class Arithmetic {
+    int num1, num2;
+    public Arithmetic(int num1, int num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public int sum(){
+        return num1+num2;
+    }
+
+    public int diff(){
+        return num1-num2;
+    }
+
+    public int product(){
+        return num1*num2;
+    }
+
+    public int divison(){
+        return num1/num2;
+    }
+
+
+
+
+
+}
